06a6ede2126f7d658db3eaa59006e9a7c84e76f6
deep mind <deepmind2021@gmail.com>
2022-06-30 11:16:46 -0400

    ejercicio 26 y 27

---
+++ src/context/AppReducer.js
@@ -0,0 +1,50 @@
+export default function appReducer(state, action) {
+  switch (action.type) {
+    case "ADD_TASK":
+      return {
+        ...state,
+        tasks: [...state.tasks, action.payload],
+      };
+    case "UPDATE_TASK": {
+      const updatedTask = action.payload;
+
+      const updatedTasks = state.tasks.map((task) => {
+        if (task.id === updatedTask.id) {
+          updatedTask.done = task.done;
+          return updatedTask;
+        }
+        return task;
+      });
+      return {
+        ...state,
+        tasks: updatedTasks,
+      };
+    }
+    case "DELETE_TASK":
+      return {
+        ...state,
+        tasks: state.tasks.filter((task) => task.id !== action.payload),
+      };
+    case "TOGGLE_TASK_DONE":
+      const updatedTasks = state.tasks.map((task) => {
+        if (task.id === action.payload) {
+          return { ...task, done: !task.done };
+        }
+        return task;
+      });
+      return {
+        ...state,
+        tasks: updatedTasks,
+      };
+
+    case "TOGGLE_TASK_FILTER":
+      const filterTasks = state.tasks.map((task) => {
+        if (task.id === action.payload) {
+          return { ...task, done: !task.done };
+        }
+        return task;
+      });
+    default:
+      return state;
+  }
+}
---
+++ src/context/GlobalState.js
@@ -0,0 +1,66 @@
+import { createContext, useReducer } from "react";
+import { v4 } from "uuid";
+
+import appReducer from "./AppReducer";
+
+const initialState = {
+  tasks: [
+    
+  ],
+};
+
+export const GlobalContext = createContext(initialState);
+
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(appReducer, initialState);
+
+  function addTask(task) {
+    dispatch({
+      type: "ADD_TASK",
+      payload: { ...task, id: v4(), done: false },
+    });
+  }
+
+  function updateTask(updatedTask) {
+    dispatch({
+      type: "UPDATE_TASK",
+      payload: updatedTask,
+    });
+  }
+
+  function deleteTask(id) {
+    dispatch({
+      type: "DELETE_TASK",
+      payload: id,
+    });
+  }
+
+  function toggleTaskDone(id) {
+    dispatch({
+      type: "TOGGLE_TASK_DONE",
+      payload: id,
+    });
+  }
+
+  function filterTask(id) {
+    dispatch({
+      type: "TOGGLE_TASK_FILTER",
+      payload: id,
+    });
+  }
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        tasks: state.tasks,
+        addTask,
+        updateTask,
+        deleteTask,
+        toggleTaskDone,
+        filterTask
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
